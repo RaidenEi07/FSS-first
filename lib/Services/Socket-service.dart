f2a7484a90061180762b0ad6bf7f275ff7e0f005
thinh <thinhquydo@gmail.com>
2024-09-11 01:18:17 +0700

    alo

--- lib/Services/Socket-service.dart
+++ lib/Services/Socket-service.dart
@@ -2,12 +2,26 @@ import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class SocketService {
   late IO.Socket socket;
+  final String baseUrl; // Define your base URL
+  final String path; // Define your path
+
+  SocketService(this.baseUrl, this.path);
 
   void connect() {
-    socket = IO.io('http://your-server-url', <String, dynamic>{
-      'transports': ['websocket'],
-      'autoConnect': false,
-    });
+    socket = IO.io(
+      baseUrl,
+      IO.OptionBuilder()
+          .setQuery({
+        '__sails_io_sdk_version': '1.2.1',
+        '__sails_io_sdk_platform': 'flutter',
+        '__sails_io_sdk_language': 'dart'
+      })
+          .setTimeout(10000) // Reduced timeout for quicker feedback
+          .setTransports(['websocket']) // for Flutter or Dart VM
+          .setPath(path)
+          .enableAutoConnect()
+          .build(),
+    );
 
     socket.connect();
 
@@ -19,6 +33,10 @@ class SocketService {
       print('Disconnected from Socket.IO server');
     });
 
+    socket.onError((error) {
+      print('Socket error: $error');
+    });
+
     // Listen for messages from the server
     socket.on('message', (data) {
       print('Message from server: $data');
@@ -27,10 +45,19 @@ class SocketService {
   }
 
   void sendMessage(String message) {
-    socket.emit('message', message);
+    if (socket.connected) {
+      socket.emit('message', message);
+    } else {
+      print('Socket is not connected. Cannot send message.');
+    }
   }
 
   void disconnect() {
-    socket.disconnect();
+    if (socket.connected) {
+      socket.disconnect();
+      print('Disconnected from Socket.IO server');
+    } else {
+      print('Socket is already disconnected.');
+    }
   }
 }
